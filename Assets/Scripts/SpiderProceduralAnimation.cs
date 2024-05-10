c4ff307cfc38cfbf992dc10256acd8862df3967c
William Rolf <williamrolf@yahoo.se>
2024-05-10 17:09:44 +0200

    (feat) Added steps for each leg
    - Added sphere "targets" for each leg
    - Added functionality in script for leg to follow their respective target (added via Unity) in order to jump to next position or take a "step".
    - leg IK targets placed offset so that legs alternate when moving forward (NEEDS WORK)

--- Assets/Scripts/SpiderProceduralAnimation.cs
+++ Assets/Scripts/SpiderProceduralAnimation.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SpiderProceduralAnimation : MonoBehaviour
 {
-    //private LayerMask floorLayer = default;        // A layer mask so that a ray can be cast just at gameobjects on the ground layer.
+    //private LayerMask floorLayer = default;        
+    // A layer mask so that a ray can be cast just at gameobjects on the ground layer.
     Vector3 currentPosition, newPosition, targetPosition, bodyPosition;
-    public float stepDistance = 1f;
+    public float stepDistance = 1.5f;
     public GameObject Target;
+    //public GameObject[] targetArray;
+    private float distanceToNearest;
 
     private void Start()
     {
@@ -18,15 +21,26 @@ public class SpiderProceduralAnimation : MonoBehaviour
     void Update()
     {
         transform.position = currentPosition;
-        targetPosition = GameObject.FindWithTag("Target").transform.position;
-        Ray ray = new Ray(targetPosition, Vector3.down);
-        if (Physics.Raycast(ray, out RaycastHit hit, 10))
+        /*targetArray = GameObject.FindGameObjectsWithTag("Target");
+        GameObject Target = targetArray[0];
+        distanceToNearest = Vector3.Distance(currentPosition, Target.transform.position);
+        for (int i = 1; i < targetArray.Length; i++)
         {
-            if (Vector3.Distance(targetPosition, currentPosition) > stepDistance)
+            float distanceToCurrent = Vector3.Distance(currentPosition, targetArray[i].transform.position);
+            if (distanceToCurrent < distanceToNearest)
             {
-            Debug.Log(Vector3.Distance(hit.point, currentPosition));
+                Target = targetArray[i];
+            }
+        }
+        */
+        Ray rayTarget = new Ray(Target.transform.position, Vector3.down);
+        if (Physics.Raycast(rayTarget, out RaycastHit info, 10))
+        {
+            if (Vector3.Distance(Target.transform.position, currentPosition) > stepDistance)
+            {
+            //Debug.Log(Vector3.Distance(hit.point, currentPosition));
             // Move the bone's position to the hit point
-            currentPosition = hit.point;
+            currentPosition = Target.transform.position;
             }
         }
     }
