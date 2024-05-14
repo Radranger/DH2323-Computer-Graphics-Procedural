a078a47644e90add470f55dae0403f024eba1ec9
William Rolf <williamrolf@yahoo.se>
2024-05-14 17:07:34 +0200

    (Update) Testing animating alternate legs
    - Added seperate IK constraint for "opposite leg" pair, which is opposite to the "leg" pair.
    - Currently NOT working, the legs update to their respective positions

--- Assets/Scripts/SpiderIKConstraint.cs
+++ Assets/Scripts/SpiderIKConstraint.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpiderProceduralAnimation : MonoBehaviour
+public class SpiderIKConstraint : MonoBehaviour
 {     
     int layerMask;
     public GameObject Target;
-    Vector3 currentPosition;
+    public GameObject oppositeTarget;
+    public GameObject[] oppositeLegs;
+    public Vector3 currentPosition;
     public float stepDistance = 1.5f;
+    public float legSpeed = 10f;
+    public bool hasMoved;
+    public SpiderOppositeIKConstraint oppositeLeg;
 
     private void Start()
     {
+        oppositeLegs = GameObject.FindGameObjectsWithTag("OppositeLeg");
         currentPosition = transform.position;
         //layerMask = LayerMask.GetMask("floor"); // A layer mask so that a ray can be cast just at gameobjects on the Floor layer.
-
+        hasMoved = false;
     }
 
     // Update is called once per frame
@@ -24,27 +30,41 @@ public class SpiderProceduralAnimation : MonoBehaviour
             {
                 //Debug.Log(Vector3.Distance(hit.point, currentPosition));
                 // Move the bone's position to the hit point
-                currentPosition = Target.transform.position;  
+                if (hasMoved == false);
+                {
+                    currentPosition = Target.transform.position;
+                    oppositeLeg.currentPosition = oppositeTarget.transform.position;
+                    oppositeLegs[0].GetComponent<SpiderOppositeIKConstraint>().hasMoved = false;
+                    oppositeLegs[1].GetComponent<SpiderOppositeIKConstraint>().hasMoved = false;
+                }
             }
-    }
 
-    /*void Update()
-        {
-            Ray ray = new Ray(body.position + (body.right * footSpacing), Vector3.down);
-            if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer.value))
+
+            /*
+            else
             {
-                transform.position = info.point;
+                isMoving = true;
+                hasMoved = false;
+                currentPosition = Vector3.Lerp(currentPosition, Target.transform.position, Time.deltaTime * legSpeed);  
             }
-        }
-    
-    Vector3 currentPosition, newPosition, targetPosition, bodyPosition;
-    public float stepDistance = 1.5f;
-    public GameObject Target;
-    //public GameObject[] targetArray;
-    private float distanceToNearest;
+            
+            if (isMoving == true)
+            {
 
-    void Update()
-    {
+            }
+            */
+    }
+        //BACKUP CODE BELOW
+
+        /*
+        Vector3 currentPosition, newPosition, targetPosition, bodyPosition;
+        public float stepDistance = 1.5f;
+        public GameObject Target;
+        //public GameObject[] targetArray;
+        private float distanceToNearest;
+
+        void Update()
+        {
         transform.position = currentPosition;
         Ray rayTarget = new Ray(Target.transform.position, Vector3.down);
         if (Physics.Raycast(rayTarget, out RaycastHit info, 10))
--- Assets/Scripts/SpiderTargetConstraint.cs
+++ Assets/Scripts/SpiderTargetConstraint.cs
@@ -14,12 +14,12 @@ public class SpiderTargetConstraint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray rayTarget = new Ray(transform.position + new Vector3(0f, 0.05f, 0f), Vector3.down);
+        Ray rayTarget = new Ray(transform.position + new Vector3(0f, 0.05f, 0f), Vector3.down); // raised the position vector in the y direction slightly, otherwise there is a risk that it will go under the plane if another game object (like parts of the sphere) is under it.
         if (Physics.Raycast(rayTarget, out RaycastHit hit, Mathf.Infinity, layerMask))
         {
             //Debug.Log(Vector3.Distance(hit.point, currentPosition));
             // Move target's position according to the hit point
-            transform.position = hit.point + new Vector3(0f, 0.01f, 0f);
-        } 
+            transform.position = hit.point;
+        }
     }
 }
